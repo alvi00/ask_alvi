86a46486d8aa910dd84dca3356b2bf11f589c63b
alvi00 <ahmadfahmid59@gmail.com>
2025-02-15 02:16:57 +0600

    changed ratelimit

--- app/tools/rateLimiting.tsx
+++ app/tools/rateLimiting.tsx
@@ -1,24 +1,30 @@
 "use server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
-import { headers } from 'next/headers';
-import { config } from '../config';
+import { headers } from "next/headers";
+import { config } from "../config";
 
 let ratelimit: Ratelimit | undefined;
 
 if (config.useRateLimiting) {
-    ratelimit = new Ratelimit({
-        redis: Redis.fromEnv(),
-        limiter: Ratelimit.slidingWindow(10, "10 m") // 10 requests per 10 minutes
-    });
+  ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(10, "10 m"), // 10 requests per 10 minutes
+  });
 }
 
 export async function checkRateLimit(streamable: any) {
-    if (config.useRateLimiting && ratelimit) {
-        const identifier = headers().get('x-forwarded-for') || headers().get('x-real-ip') || headers().get('cf-connecting-ip') || headers().get('client-ip') || "";
-        const { success } = await ratelimit.limit(identifier);
-        streamable.done({ 'status': 'rateLimitReached' });
-        return success;
-    }
-    return true;
+  if (config.useRateLimiting && ratelimit) {
+    const headersObject = await headers(); // Await headers() to get the actual headers object
+    const identifier =
+      headersObject.get("x-forwarded-for") ||
+      headersObject.get("x-real-ip") ||
+      headersObject.get("cf-connecting-ip") ||
+      headersObject.get("client-ip") ||
+      "";
+    const { success } = await ratelimit.limit(identifier);
+    streamable.done({ status: "rateLimitReached" });
+    return success;
+  }
+  return true;
 }
